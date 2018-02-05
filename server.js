db08112fa710c00f088a8e438680345c148aa724
Benjamin Smith <benjaminsmith@Benjamins-MacBook-Pro.local>
2018-02-05 17:51:41 -0500

    initial commit

---
+++ server.js
@@ -0,0 +1,68 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+
+
+app.use((req, res, next) => {
+  var now = new Date().toString();
+  var log = `${now}: ${req.method} ${req.url}`;
+
+  console.log(log);
+  fs.appendFile('server.log', log + '\n', (err) => {
+    if (err) {
+      console.log('unable to append server.log');
+    }
+  });
+  next();
+});
+
+app.use((req, res, next) => {
+//  res.send('<h1>hello express</h1>');
+  res.render('maintain.hbs', {
+  });
+});
+
+app.use(express.static(__dirname + '/public'));
+
+
+hbs.registerHelper('getCurrentYear', () => {
+  return new Date().getFullYear()
+});
+
+hbs.registerHelper('screamIt', (text) => {
+  return text.toUpperCase()
+});
+
+app.get('/', (req, res) => {
+//  res.send('<h1>hello express</h1>');
+  res.render('index.hbs', {
+    pageTitle: 'home page',
+    welcomeNote: 'welcome to my page baud.',
+    currentYear: new Date().getFullYear()
+  });
+});
+
+
+app.get('/about', (req, res) => {
+  res.render('about.hbs', {
+    pageTitle: 'about page',
+    currentYear: new Date().getFullYear()
+  });
+
+});
+
+app.get('/bad', (req, res) => {
+  res.send({
+    error: 'you could not get good data.'
+  });
+});
+
+
+app.listen(3000, () => {
+  console.log('server is up on 3000');
+});
